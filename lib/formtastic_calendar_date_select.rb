ec9fc4cbf88f04def05e4ddbe823bf2054a9ef46
Robert Pankowecki <rpa@gavdi.com>
2010-07-12 17:07:43 +0200

    Rails 3.

--- lib/formtastic_calendar_date_select.rb
+++ lib/formtastic_calendar_date_select.rb
@@ -1,13 +1,20 @@
 module DavidOshiro
-	module Formtastic
-		module CalendarDateSelect
-			protected
-			
-			def calendar_input(method, options = {})
-				self.label(method, options_for_label(options)) +
-				self.calendar_date_select(method, strip_formtastic_options(options))
-			end
-			
-		end
-	end
+  module Formtastic
+    module CalendarDateSelect
+
+      class Railtie < ::Rails::Railtie
+        initializer 'formtastic.calendardateselect.initialize', :after => ['calendardateselect.initialize', 'formtastic.initialize'] do      
+          Formtastic::SemanticFormBuilder.send(:include, DavidOshiro::Formtastic::CalendarDateSelect)
+        end
+      end
+
+      protected
+		
+      def calendar_input(method, options = {})
+        self.label(method, options_for_label(options)) +
+          self.calendar_date_select(method, strip_formtastic_options(options))
+      end
+
+    end
+  end
 end
